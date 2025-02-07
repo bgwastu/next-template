f8e3b1d1811264475ba6720ff0ceb40bea097b2f
Bagas Wastu <67826350+bgwastu@users.noreply.github.com>
2025-02-07 14:27:58 +0700

    chore: update process-compose and init-db script for improved database setup

--- config/init-db.sh
+++ config/init-db.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
+set -e # Exit on error
+
 DB_NAME=local
 PGHOST=localhost
+
+# If data directory does not exist or is empty, create it
+if [ ! -d ".devbox/virtenv/postgresql/data" ] || [ -z "$(ls -A .devbox/virtenv/postgresql/data)" ]; then
+  echo "[Postgres] Creating data directory..."
+  mkdir -p ".devbox/virtenv/postgresql/data" || { echo "Failed to create data directory"; exit 1; }
+  initdb -D ".devbox/virtenv/postgresql/data" || { echo "Failed to initialize database"; exit 1; }
+  echo "[Postgres] Done"
+fi
 
 # DB does not exist
 if ! psql -h $PGHOST -lqt | cut -d \| -f 1 | grep -qw $DB_NAME; then
   echo "[Postgres] Creating database..."
-  createdb -h $PGHOST $DB_NAME
+  createdb -h $PGHOST $DB_NAME || { echo "Failed to create database"; exit 1; }
   echo "[Postgres] Done"
 fi
 
 # `postgres` user does not exist
 if ! psql -h $PGHOST -d $DB_NAME -c "SELECT 1 FROM pg_roles WHERE rolname='postgres'" | grep -q 1; then
   echo "[Postgres] Setting permissions..."
-  psql -h $PGHOST -d $DB_NAME -f config/db-permissions.sql
+  psql -h $PGHOST -d $DB_NAME -f config/db-permissions.sql || { echo "Failed to set permissions"; exit 1; }
   echo "[Postgres] Done"
 fi
